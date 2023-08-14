e4eadd0660625a02e202371ec23daee281d780b8
Ashish <ashishgupta2972002@gmail.com>
2023-08-14 20:53:47 +0530

    215-kth-largest-element-in-an-array

---
+++ competitive_programing/leetcode/215_kth_largest_element_in_array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int size=nums.size();
+        sort(nums.begin(),nums.end());
+       
+            return nums[size-k];
+        
+    }
+};            
