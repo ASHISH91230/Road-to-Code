ef7e8ab6faf34d521f41eb46c003952b0906101d
Ashish <ashishgupta2972002@gmail.com>
2023-08-19 21:41:56 +0530

    insert node at the begining

---
+++ coding studio/Insert_Node_At_The_Beginning.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    // Write your code here
+    Node*temp=new Node;
+    temp->data=newValue;
+    temp->next=list;
+    list=temp;
+
+    
+}
