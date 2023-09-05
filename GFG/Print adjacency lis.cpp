9be7a2ff5c642841aec8f9de044ce5995945a182
Ashish <ashishgupta2972002@gmail.com>
2023-09-05 21:26:22 +0530

    print adjency list

---
+++ GFG/Print adjacency lis.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
+        // Code here
+        vector<vector<int>> ans(V);
+        for(int i = 0; i < edges.size(); i++){
+            int a=edges[i].first;
+            int b=edges[i].second;
+            ans[a].push_back(b);
+            ans[b].push_back(a);
+        }
+        return ans;
+    }
+};
