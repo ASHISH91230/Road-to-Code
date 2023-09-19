8f8c01f894f7b6bbec57b52101b38579207acf3d
Ashish <ashishgupta2972002@gmail.com>
2023-09-20 00:46:34 +0530

    152. Maximum Product Subarray

--- competitive_programing/leetcode/152_maximum product subarray.cpp
+++ competitive_programing/leetcode/152_maximum product subarray.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int c=nums[0];
-        int max=c;
-        for(int i=0;i<nums.size()-1;i++)
+        double c=1;
+        int max=INT_MIN;
+        if(nums.size()==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.size();i++)
         {
-            
-            c=c*nums[i+1];
-             if(c>max)
-                {
-                    max=c;
-                }
-
-            if(c==0){
-                c=nums[i];
+           
+            c=c*nums[i];
+            if(c>max)
+            {
                 max=c;
+            } 
+      
+            if(c==0)
+            {
+                c=1;
             }
+
+            
+        }
+        cout<<max;
+        c=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+           
+           
+            c=c*nums[i];
+            if(c>max)
+            {
+                max=c;
+            } 
+          
+            if(c==0)
+            {
+                c=1;
+            }
+
+            
         }
         
         return max;
